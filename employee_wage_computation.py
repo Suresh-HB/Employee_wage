a9b039223faebd5866294f25a94e3cbe37ec43d5
Suresh HB <sureshsai175475@gmail.com>
2024-07-20 16:27:59 +0530

    [Suresh] Add: Adding function to calculate monthly wage based on employment type for employee

--- employee_wage_computation.py
+++ employee_wage_computation.py
@@ -15,7 +15,7 @@ WAGE_PER_HOUR = 20
 FULL_DAY_WORKING_HOURS = 8
 PART_TIME_WAGE = 15
 PART_TIME_WORKING_HOURS = 8
-WORKING_DAYS_PER_MONTH = 20
+
 
 def check_attendance():
 
@@ -28,15 +28,12 @@ def check_attendance():
         employee_attendance
     """    
 
-
     attendance = random.randint(0, 1)
 
     if attendance == 1:
         employee_attendance = 'present'
-
     else:
         employee_attendance = 'absent'
-    
     return employee_attendance
 
 
@@ -53,31 +50,56 @@ def employee_type_wage_per_day(type_of_employment):
 
     """    
 
-    if type_of_employment == 'FULL-TIME':
+    if type_of_employment == 1:
         employee_wage_per_day = WAGE_PER_HOUR * FULL_DAY_WORKING_HOURS
-        return employee_wage_per_day
-    
-    elif type_of_employment == 'PART-TIME':
-        employee_wage_per_day = PART_TIME_WAGE * PART_TIME_WORKING_HOURS
-        return employee_wage_per_day
+        return employee_wage_per_day    
     
     else:
-        return "enter valid employee_type(FULL-TIME/PART-TIME)"
+        employee_wage_per_day = PART_TIME_WAGE * PART_TIME_WORKING_HOURS
+        return  employee_wage_per_day
     
+
+def calculate_employee_monthly_wage():
+
+    """
+    description:
+        This function is used to calculate monthly wage.
+    parameters:
+        None
+    return:
+        employee_monthly wage
+    """
+
+    employee_atd_lst = []
+    employee_daily_wage =[]
+    working_days = 1
+    total_wage = 0
+
+    while working_days<=20:
+
+        attendance = check_attendance()
+        employee_atd_lst.append(attendance)
+
+        if attendance == 'present':
+
+            emp_type = random.randint(0,1)
+            one_day_wage = employee_type_wage_per_day(emp_type)
+            employee_daily_wage.append(one_day_wage)
+            total_wage+=one_day_wage
+        else:
+            employee_daily_wage.append(0)
+            total_wage+=0
+        working_days+=1
+    return total_wage, employee_atd_lst, employee_daily_wage
+
 
 def main():
+    
+    total_wage, employee_atd_lst, employee_daily_wage = calculate_employee_monthly_wage()
+    print(f'Employee 20 days attendance is {employee_atd_lst}')
+    print(f'Employee daily wage for 20 days is {employee_daily_wage}')
+    print(f'Total monthly wage of employee is {total_wage}') 
 
-    attendance = check_attendance()
-    print("Employee is ",attendance)
-    wage_per_day = 0
 
-    if attendance == 'present':
-        type_of_employment = input("enter FULL-TIME/PART-TIME: ").upper()
-        wage_per_day = employee_type_wage_per_day(type_of_employment)
-        print(f"employee wage  = {wage_per_day}")
-    else:
-        print(f"employee wage  = {wage_per_day}")
-
-        
 if __name__ == '__main__':
     main()
